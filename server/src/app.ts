c3c68b627a3fb8ef2987028af01092cb3755ee87
AbhayShaw0301 <abhayshaw0301@gmail.com>
2024-02-05 11:29:23 +0530

    express http error handling

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,6 +3,7 @@ import express, {NextFunction} from "express";
 import morgan from "morgan";
 
 import notesRoutes from "../src/routes/notes";
+import createHttpError, {isHttpError} from "http-errors";
 
 const app = express();
 app.use(morgan("dev"));
@@ -10,16 +11,21 @@ app.use(express.json());
 
 app.use("/api/notes",notesRoutes);
 app.use((req, res, next) => {
-   next(Error( "Endpoint not found"));
+   next(createHttpError(404, "Endpoint not found"));
 });
 
 // @ts-ignore
 app.use((error:unknown,req:Request,res:Response,next:NextFunction)=>{
    console.error(error);
    let errorMessage="An unkown error occurred";
-   if(error instanceof Error) errorMessage=error.message;
+   let statusCode=500;
+   if(isHttpError(error))
+   {
+      statusCode=error.status;
+      errorMessage=error.message;
+   }
    // @ts-ignore
-   res.status(500).json({error:errorMessage})
+   res.status(statusCode).json({error:errorMessage})
 
 });
 
--- server/src/controller/notes.ts
+++ server/src/controller/notes.ts
@@ -1,39 +1,56 @@
 import NoteModel from "../models/note";
 import {RequestHandler} from "express";
+import createHttpError from "http-errors";
+import mongoose from "mongoose";
+import notes from "../routes/notes";
 
-export const getNotes:RequestHandler =async(req,res,next)=>{
-    try{
+export const getNotes: RequestHandler = async (req, res, next) => {
+    try {
         const notes = await NoteModel.find().exec();
         res.status(200).json(notes);
-    }catch (error) {
+    } catch (error) {
         next(error);
 
     }
 
 };
 
-export const getNote:RequestHandler=async (req,res,next)=>{
-    const noteId=req.params.noteId;
-    try{
+export const getNote: RequestHandler = async (req, res, next) => {
+    const noteId = req.params.noteId;
+    try {
+        if(!mongoose.isValidObjectId(noteId)){
+            throw createHttpError(400,"Invalid note id.")
+        }
         const note = await NoteModel.findById(noteId).exec();
+        if(!note){
+            throw createHttpError(404,"Note not found.");
+        }
+
         res.status(200).json(note);
-    }catch (error){
+    } catch (error) {
         next(error);
     }
 }
 
+interface createNoteBody {
+    title?: string,
+    text?: string
+}
 
-export const createNotes:RequestHandler=async (req,res,next)=>{
-    const title =req.body.title;
-    const text =req.body.text;
-    try{
-        const newNote= await NoteModel.create({
-            title:title,
-            text:text,
+export const createNotes: RequestHandler<unknown,unknown,createNoteBody,unknown> = async (req, res, next) => {
+    const title = req.body.title;
+    const text = req.body.text;
+    try {
+        if(!title){
+            throw createHttpError(400,"Note must have a title");
+        }
+        const newNote = await NoteModel.create({
+
+            title: title,
+            text: text,
         });
-res.status(201).json(newNote);
-    }catch (error)
-    {
+        res.status(201).json(newNote);
+    } catch (error) {
         next(error);
     }
 };
