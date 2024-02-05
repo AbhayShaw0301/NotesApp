63b1c4cd93b79160176a9448a774cac7d0b7cd27
AbhayShaw0301 <abhayshaw0301@gmail.com>
2024-02-05 18:42:34 +0530

    fixed note fetch and styling the notes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,21 @@
 import React, {useEffect, useState} from 'react';
-import logo from './logo.svg';
+import * as NotesApi from "../src/network/notes_api";
 
 import {Button, Col, Container, Row} from "react-bootstrap";
 import {Note as NoteModel} from "./models/note";
 import Note from "./components/Note";
+import styleUtils from "../src/styles/utils.module.css"
 import styles from "../src/styles/NotesPage.module.css";
+import AddNoteDialog from "./components/AddNoteDialog";
+
 function App() {
     const [notes, setNotes] = useState<NoteModel[]>([]);
+    const [showAddNoteDialog, setShowAddNoteDialog] = useState(false);
     useEffect(() => {
         async function loadNotes() {
             try {
-                const response = await fetch("http://localhost:5000/api/notes", {method: "GET"});
-                const notes = await response.json();
+                const notes = await NotesApi.fetchNotes();
+
                 setNotes(notes);
             } catch (error) {
                 console.error(error);
@@ -24,14 +28,27 @@ function App() {
     }, []);
     return (
         <Container>
+            <Button className={`mb-4 ${styleUtils.blockCenter}`}
+                onClick={() => setShowAddNoteDialog(true)}>
+                Add New Note
+            </Button>
             <Row xs={1} md={2} xl={3} className="g-4">
                 {notes.map(note => (
                     <Col key={note._id}>
-                        <Note note={note} className={styles.note} />
+                        <Note note={note} className={styles.note}/>
                     </Col>
 
                 ))}
             </Row>
+            {
+                showAddNoteDialog &&
+                <AddNoteDialog onDismiss={() => setShowAddNoteDialog(false)}
+                               onNoteSaved={(newNote) => {
+                                   setNotes([...notes,newNote])
+                                   setShowAddNoteDialog(false)
+                               }}/>
+
+            }
         </Container>
     );
 }
