7c735cc91f06ab79c48a4db9a03da6fb31390408
AbhayShaw0301 <abhayshaw0301@gmail.com>
2024-02-05 10:19:39 +0530

    setting up errorHandler

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,33 @@
 import "dotenv/config";
-import express from "express";
+import express, {NextFunction} from "express";
+import NoteModel from "../src/models/note";
+
 
 const app = express();
 
+app.get("/",async(req,res,next)=>{
+   try{
+      const notes = await NoteModel.find().exec();
+      res.status(200).json(notes);
+   }catch (error) {
+      next(error);
+
+   }
+
+});
+app.use((req, res, next) => {
+   next(Error( "Endpoint not found"));
+});
+
+// @ts-ignore
+app.use((error:unknown,req:Request,res:Response,next:NextFunction)=>{
+   console.error(error);
+   let errorMessage="An unkown error occurred";
+   if(error instanceof Error) errorMessage=error.message;
+   // @ts-ignore
+   res.status(500).json({error:errorMessage})
+
+});
 
 
 
